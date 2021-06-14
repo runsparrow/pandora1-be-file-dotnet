0e747771c5d4f42312f6782eff06c8186393a250
Sheng.Xu <949525088@qq.com>
2021-06-14 23:56:55 +0800

    v1
    v1

---
+++ pandora1-be-file-dotnet/Models/Dto/FileDownloadDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pandora1_be_file_dotnet.Models.Dto
+{
+    public class FileDownloadDto
+    {
+        public string FileUrl { get; set; }
+    }
+}
