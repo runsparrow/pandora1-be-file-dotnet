885de717e603a5934b12b4fed635dd9481f2df68
Sheng.Xu <949525088@qq.com>
2021-11-29 16:57:03 +0800

    v1
    v1

--- pandora1-be-file-dotnet/Controllers/FileController.cs
+++ pandora1-be-file-dotnet/Controllers/FileController.cs
@@ -11,7 +11,6 @@ using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -29,11 +28,8 @@ namespace pandora1_be_file_dotnet.Controllers
         private readonly ILogger<FileController> _logger;
         private RestClient _client;
         private readonly IHttpContextAccessor _accessor;
-        private static PixelFormat[] indexedPixelFormats = { PixelFormat.Undefined, PixelFormat.DontCare,
-                PixelFormat.Format16bppArgb1555, PixelFormat.Format1bppIndexed, PixelFormat.Format4bppIndexed,
-                PixelFormat.Format8bppIndexed};
 
-        public FileController(ILogger<FileController> logger, IHttpContextAccessor accessor)
+        public FileController(ILogger<FileController> logger,IHttpContextAccessor accessor)
         {
             _logger = logger;
             _accessor = accessor;
@@ -49,7 +45,7 @@ namespace pandora1_be_file_dotnet.Controllers
             string suffix = Path.GetExtension(fileName);
             int isImage = 1;
             string fileAppSettingPath = "";
-            if (allowViewSuffixAr.IndexOf(suffix) != -1)
+            if (allowViewSuffixAr.IndexOf(suffix)!=-1)
             {
                 isImage = 0;
                 fileAppSettingPath = Appsettings.app(new string[] { "UploadFilePath", "VideoPath" });
@@ -62,7 +58,7 @@ namespace pandora1_be_file_dotnet.Controllers
             var yearDir = DateTime.Now.ToString("yyyy");
             var monthDir = DateTime.Now.ToString("MM");
             var dayDir = DateTime.Now.ToString("dd");
-            string envPath = Path.Combine(rootDir, fileAppSettingPath, yearDir, monthDir, dayDir);
+            string envPath = Path.Combine(rootDir , fileAppSettingPath, yearDir, monthDir, dayDir);
             var dir = tempDir;
             var files = Directory.GetFiles(dir);
             var finalDir = envPath;
@@ -71,7 +67,7 @@ namespace pandora1_be_file_dotnet.Controllers
                 Directory.CreateDirectory(finalDir);
             }
             var finalPath = Path.Combine(finalDir, fileName);
-
+      
             using (var fs = new FileStream(finalPath, FileMode.Create))
             {
                 var fileParts = files.OrderBy(x => x.Length).ThenBy(x => x);
@@ -96,43 +92,23 @@ namespace pandora1_be_file_dotnet.Controllers
                 {
                     //pic
                     dto.dpi = img.Width + "*" + img.Height;
-                    if (IsPixelFormatIndexed(img.PixelFormat))
+                    using (var graphic = Graphics.FromImage(img))
                     {
-                        Bitmap bmp = new Bitmap(img);
                         var font = new Font(FontFamily.GenericSansSerif, 800, FontStyle.Bold, GraphicsUnit.Pixel);
                         var color = Color.FromArgb(128, 255, 255, 255);
                         var brush = new SolidBrush(color);
                         var point = new Point((img.Width / 2) - ((img.Width / 2) / 2) - 20, (img.Height / 2) - ((img.Height / 2) / 2));
-                        System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(bmp);
-                        g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-                        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-                        g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
-                        g.DrawImage(bmp, 0, 0);
 
-                        g.DrawString("t-pic.cn", font, brush, point);
-                        g.Dispose();
+                        graphic.DrawString("", font, brush, point);
                     }
-                    else
-                    {
-                        using (var graphic = Graphics.FromImage(img))
-                        {
-                            var font = new Font(FontFamily.GenericSansSerif, 800, FontStyle.Bold, GraphicsUnit.Pixel);
-                            var color = Color.FromArgb(128, 255, 255, 255);
-                            var brush = new SolidBrush(color);
-                            var point = new Point((img.Width / 2) - ((img.Width / 2) / 2) - 20, (img.Height / 2) - ((img.Height / 2) / 2));
-
-                            graphic.DrawString("t-pic.cn", font, brush, point);
-                        }
-                    }
-                  
                     finalPath = finalPath.Replace(fileName, "$" + fileName);
                     img.Save(finalPath);
                 }
 
-
+               
                 dto.status = new StatusProxyDto();
                 dto.name = fileName;
-                dto.size = bytesLength + "";
+                dto.size = bytesLength+"";
                 dto.classifyName = "";
                 dto.isImage = isImage;
                 dto.ext = fileName.Substring(fileName.LastIndexOf(".") + 1);
@@ -141,28 +117,17 @@ namespace pandora1_be_file_dotnet.Controllers
 
 
                 dto.url = "/" + finalPath.Substring(finalPath.IndexOf("uploadFiles")).Replace("\\", "/");
-
+              
                 RestRequest request = new RestRequest("/MIS/CMS/MemberAction/Upload", Method.POST);
                 string token = _accessor.HttpContext.Request.Headers["Authorization"];
                 token = token.Replace("Bearer ", "");
                 dto.memberId = AuthHelper.GetClaimFromToken(token).Id;
-                dto.memberName = dto.ownerName = AuthHelper.GetClaimFromToken(token).Name;
-                _logger.LogInformation(dto.memberId + "||" + dto.memberName);
-                _client.AddDefaultHeader("Authorization", "Bearer " + token);
+                dto.memberName = dto.ownerName  = AuthHelper.GetClaimFromToken(token).Name;
+                _logger.LogInformation(dto.memberId+"||"+dto.memberName);
+                _client.AddDefaultHeader("Authorization","Bearer "+ token);
                 request.AddJsonBody(dto);
-                var res = await _client.ExecuteAsync(request);
+                var res=await _client.ExecuteAsync(request);
             }
-        }
-
-        [NonAction]
-        private static bool IsPixelFormatIndexed(PixelFormat imgPixelFormat)
-        {
-            foreach (PixelFormat pf in indexedPixelFormats)
-            {
-                if (pf.Equals(imgPixelFormat)) return true;
-            }
-
-            return false;
         }
 
         [HttpPost]
@@ -171,7 +136,7 @@ namespace pandora1_be_file_dotnet.Controllers
             //List<string> allowPicSuffixAr = new List<string> { ".jpg", ".png", ".jpeg", ".gif",".bmp" };
             //List<string> allowViewSuffixAr = new List<string> { ".mp4", ".mkv", ".mov", ".m4v",".wmv",".avi" , ".flv" };
             ApiResponse<FileResponseDto> response = new ApiResponse<FileResponseDto>();
-            if (Request.ContentLength == 0)
+            if (Request.ContentLength==0)
             {
                 throw new ServiceException(ErrorDescriptor.FILE_NULL);
             }
@@ -202,13 +167,13 @@ namespace pandora1_be_file_dotnet.Controllers
             }
             var filePath = Path.Combine(dir, index.ToString());
 
-
+            
             var filePathWithFileName = string.Concat(filePath, fileName);
             using (var stream = new FileStream(filePathWithFileName, FileMode.Create))
             {
                 await file.CopyToAsync(stream);
             }
-
+           
             var fileResponseDto = new FileResponseDto();
             if (index == maxChunk - 1)
             {
@@ -220,7 +185,7 @@ namespace pandora1_be_file_dotnet.Controllers
         }
 
         [HttpPost]
-        public async Task<ApiResponse<SingleFileResponseDto>> Upload_Single_File(IFormFile file, [FromServices] IWebHostEnvironment environment)
+        public async Task<ApiResponse<SingleFileResponseDto>> Upload_Single_File(IFormFile file,[FromServices] IWebHostEnvironment environment)
         {
             ApiResponse<SingleFileResponseDto> response = new ApiResponse<SingleFileResponseDto>();
             string suffix = Path.GetExtension(file.FileName);
@@ -271,9 +236,9 @@ namespace pandora1_be_file_dotnet.Controllers
                     var font = new Font(FontFamily.GenericSansSerif, 800, FontStyle.Bold, GraphicsUnit.Pixel);
                     var color = Color.FromArgb(128, 255, 255, 255);
                     var brush = new SolidBrush(color);
-                    var point = new Point((img.Width / 2) - ((img.Width / 2) / 2) - 20, (img.Height / 2) - ((img.Height / 2) / 2));
+                    var point = new Point((img.Width/2)- ((img.Width / 2)/2)-20, (img.Height / 2) - ((img.Height / 2) / 2));
 
-                    graphic.DrawString("t-pic.cn", font, brush, point);
+                    graphic.DrawString("", font, brush, point);
                 }
                 filePathWithFileName = filePathWithFileName.Replace(uniqueFileName, "$" + uniqueFileName);
                 img.Save(filePathWithFileName);
@@ -299,7 +264,7 @@ namespace pandora1_be_file_dotnet.Controllers
             //request.AddJsonBody(dto);
             //var res = await _client.ExecuteAsync(request);
 
-            response.Data = new SingleFileResponseDto { RelativePath = Appsettings.app(new string[] { "UploadFilePath", "Uri" }) + returnToRelativePath + "/" + "$" + uniqueFileName, FileName = uploadFIle.FileName, Dpi = dpi };
+            response.Data = new SingleFileResponseDto { RelativePath = Appsettings.app(new string[] { "UploadFilePath", "Uri" })+returnToRelativePath+"/"+ "$" + uniqueFileName, FileName = uploadFIle.FileName,Dpi= dpi };
             return response;
         }
 
@@ -372,7 +337,7 @@ namespace pandora1_be_file_dotnet.Controllers
             //request.AddJsonBody(dto);
             //var res = await _client.ExecuteAsync(request);
 
-            response.Data = new SingleFileResponseDto { RelativePath = Appsettings.app(new string[] { "UploadFilePath", "Uri" }) + returnToRelativePath + "/" + uniqueFileName, FileName = uploadFIle.FileName, Dpi = dpi };
+            response.Data = new SingleFileResponseDto { RelativePath = Appsettings.app(new string[] { "UploadFilePath", "Uri" }) + returnToRelativePath + "/"  + uniqueFileName, FileName = uploadFIle.FileName, Dpi = dpi };
             return response;
         }
 
@@ -380,7 +345,7 @@ namespace pandora1_be_file_dotnet.Controllers
         public FileStreamResult FileDownload(FileDownloadDto fileDto, [FromServices] IWebHostEnvironment environment)
         {
             string foldername = "";
-            string filepath = Path.Combine(environment.WebRootPath, fileDto.FileUrl.Replace("$", ""));
+            string filepath = Path.Combine(environment.WebRootPath, fileDto.FileUrl.Replace("$",""));
             var stream = System.IO.File.OpenRead(filepath);
             string fileExt = fileDto.FileUrl.Substring(fileDto.FileUrl.LastIndexOf('.'));  // 这里可以写一个获取文件扩展名的方法，获取扩展名
             //获取文件的ContentType
